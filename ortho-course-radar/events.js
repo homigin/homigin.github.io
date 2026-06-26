85b28b6a2f80d3f71dc7923b2e4276b0e386e296
homigin <homigin@users.noreply.github.com>
2026-06-26 15:33:24 +0000

    deploy: 6b06187616c42b966b79870bf4f372bdad9882bc

---
+++ ortho-course-radar/events.js
@@ -0,0 +1,202 @@
+window.ORTHO_EVENTS = [
+  {
+    "date": "2026-05-28",
+    "title": "2026/05/28-05/31 IRCAD Taiwan Arthroscopy course (wrist and elbow)",
+    "source": "TSSH 台灣手外科醫學會",
+    "cat": "運動醫學",
+    "place": "原公告",
+    "url": "https://handsurgery.com.tw/2026-05-28-05-31-ircad-taiwan-arthroscopy-course-wrist-and-elbow/"
+  },
+  {
+    "date": "2026-06-13",
+    "title": "2026.06.13 2026年中華民國關節鏡及膝關節醫學會年會-雙城論壇, Tokyo vs. Taipei",
+    "source": "台灣關節鏡及膝關節醫學會",
+    "cat": "運動醫學",
+    "place": "原公告",
+    "url": "https://www.taiwanarthroscopy.org.tw/activity/2026-06-13-2026%e5%b9%b4%e4%b8%ad%e8%8f%af%e6%b0%91%e5%9c%8b%e9%97%9c%e7%af%80%e9%8f%a1%e5%8f%8a%e8%86%9d%e9%97%9c%e7%af%80%e9%86%ab%e5%ad%b8%e6%9c%83%ef%a6%8e%e6%9c%83/"
+  },
+  {
+    "date": "2026-06-14",
+    "title": "2026.06.14 大體膝關節鏡實作研習會 Basic Knee Cadaver Course",
+    "source": "台灣關節鏡及膝關節醫學會",
+    "cat": "運動醫學",
+    "place": "原公告",
+    "url": "https://www.taiwanarthroscopy.org.tw/conference/2026-06-14-%e5%a4%a7%e9%ab%94%e8%86%9d%e9%97%9c%e7%af%80%e9%8f%a1%e5%af%a6%e4%bd%9c%e7%a0%94%e7%bf%92%e6%9c%83-basic-knee-cadaver-course/"
+  },
+  {
+    "date": "2026-06-27",
+    "title": "Taiwan Shoulder and Elbow Society【2026 Knee Arthroscopic Workshop Course】",
+    "source": "TOA 台灣骨科醫學會",
+    "cat": "運動醫學",
+    "place": "國立成功大學醫學院 臨床手術技能發展創新教室",
+    "url": "https://bone.org.tw/education/events/11519/"
+  },
+  {
+    "date": "2026-06-27",
+    "title": "臺中榮民總醫院骨科部【微創脊椎內視鏡手術動物實作工作坊】",
+    "source": "TOA 台灣骨科醫學會",
+    "cat": "脊椎",
+    "place": "臺中榮民總醫院教學大樓四樓",
+    "url": "https://bone.org.tw/education/events/11513/"
+  },
+  {
+    "date": "2026-06-27",
+    "title": "臺北榮總骨科部 中華醫學會【2026第三屆脊柱側彎關懷月研討會： 矢狀面曲線的重要性，術後快速恢復暨手術工作坊】",
+    "source": "TOA 台灣骨科醫學會",
+    "cat": "脊椎",
+    "place": "上午：臺北榮民總醫院 致德樓第十會議室 下午：中正樓三樓麻醉部會議室及手術房",
+    "url": "https://bone.org.tw/education/events/11494/"
+  },
+  {
+    "date": "2026-06-27",
+    "title": "(VIPS-Essentials 2026) – Learning from the master 115.06.27(共1小時20分)、115.07.28(共4時30分)(1.3/2.3點)",
+    "source": "復健醫學會 MSK / sports / pain",
+    "cat": "疼痛",
+    "place": "原公告",
+    "url": "https://www.pmr.org.tw/active_news/active_info.asp?/4371.html"
+  },
+  {
+    "date": "2026-06-28",
+    "title": "台灣區域麻醉暨止痛醫學會【疼痛擂台6：真實病人工作坊-腰臀同源】",
+    "source": "TOA 台灣骨科醫學會",
+    "cat": "疼痛",
+    "place": "中山醫學大學 誠愛樓10樓",
+    "url": "https://bone.org.tw/education/events/11520/"
+  },
+  {
+    "date": "2026-06-28",
+    "title": "疼痛擂台6：真實病人工作坊-腰臀同源115.06.28(共6時20分)(4點)",
+    "source": "復健醫學會 MSK / sports / pain",
+    "cat": "疼痛",
+    "place": "原公告",
+    "url": "https://www.pmr.org.tw/active_news/active_info.asp?/4423.html"
+  },
+  {
+    "date": "2026-07-04",
+    "title": "台灣人工關節教育學會【TAES Annual Current Concepts in Knee Joint Replacement】",
+    "source": "TOA 台灣骨科醫學會",
+    "cat": "關節重建",
+    "place": "臺北晶華酒店 3F 宴會廳",
+    "url": "https://bone.org.tw/education/events/11500/"
+  },
+  {
+    "date": "2026-07-04",
+    "title": "新竹臺大分院骨科部【桃竹苗地區骨科學術研討論】",
+    "source": "TOA 台灣骨科醫學會",
+    "cat": "綜合",
+    "place": "和選旅THE HO HOTEL飯店一樓會議廳",
+    "url": "https://bone.org.tw/education/events/11533/"
+  },
+  {
+    "date": "2026-07-04",
+    "title": "高醫附院骨科部【AI時代的臨床研究：高效研究者養成與無程式碼研究計畫實作工作坊】",
+    "source": "TOA 台灣骨科醫學會",
+    "cat": "研究",
+    "place": "高醫附院啟川大樓13ES骨科部討論室",
+    "url": "https://bone.org.tw/education/events/11528/"
+  },
+  {
+    "date": "2026-07-05",
+    "title": "高高屏骨科聯誼會【肌關算盡，髖韌有餘】",
+    "source": "TOA 台灣骨科醫學會",
+    "cat": "綜合",
+    "place": "高雄福華大飯店七樓 金鳳廳",
+    "url": "https://bone.org.tw/education/events/11524/"
+  },
+  {
+    "date": "2026-07-10",
+    "title": "埔基醫療財團法人埔里基督教醫院【創傷休克】",
+    "source": "TOA 台灣骨科醫學會",
+    "cat": "創傷",
+    "place": "埔基醫院-8樓謝緯紀念堂",
+    "url": "https://bone.org.tw/education/events/11526/"
+  },
+  {
+    "date": "2026-07-18",
+    "title": "中華民國小兒骨科醫學會【2026 台灣DDH疾病認證交流與學術討論會暨中華民國小兒骨科醫學會年會】",
+    "source": "TOA 台灣骨科醫學會",
+    "cat": "綜合",
+    "place": "高雄醫學大學附設高醫岡山醫院",
+    "url": "https://bone.org.tw/education/events/11507/"
+  },
+  {
+    "date": "2026-07-18",
+    "title": "台灣脊椎微創內視鏡醫學會【2026 脊椎微創內視鏡醫學會仿真模型手術體驗工作坊-高雄長庚場】",
+    "source": "TOA 台灣骨科醫學會",
+    "cat": "脊椎",
+    "place": "高雄長庚醫院 醫學大樓外科第二會議室",
+    "url": "https://bone.org.tw/education/events/11535/"
+  },
+  {
+    "date": "2026-07-19",
+    "title": "中華民國骨質疏鬆症學會【骨質疏鬆症專科醫師學分班】",
+    "source": "TOA 台灣骨科醫學會",
+    "cat": "骨鬆",
+    "place": "中山醫學大學正心樓7樓0713教室",
+    "url": "https://bone.org.tw/education/events/11532/"
+  },
+  {
+    "date": "2026-07-19",
+    "title": "【5/19報名】2026/07/19骨鬆專科醫師學分班(台中)(僅限本會醫師報名)",
+    "source": "台灣骨質疏鬆症學會",
+    "cat": "骨鬆",
+    "place": "原公告",
+    "url": "https://www.toa1997.org.tw/events/content.php?id=606&t=0"
+  },
+  {
+    "date": "2026-07-19",
+    "title": "【5/19報名】2026/07/19骨鬆衛教師認證課程及考試(台中)(僅限本會醫事人員報名)",
+    "source": "台灣骨質疏鬆症學會",
+    "cat": "骨鬆",
+    "place": "原公告",
+    "url": "https://www.toa1997.org.tw/events/content.php?id=604&t=0"
+  },
+  {
+    "date": "2026-07-25",
+    "title": "【歡迎報名】2026/07/25 2026年骨鬆管理新座標-北區場 精準順序療法與病患中心",
+    "source": "台灣骨質疏鬆症學會",
+    "cat": "骨鬆",
+    "place": "原公告",
+    "url": "https://www.toa1997.org.tw/events/content.php?id=622&t=0"
+  },
+  {
+    "date": "2026-07-26",
+    "title": "2026.07.26 住院醫師聯合教育訓練課程",
+    "source": "台灣關節鏡及膝關節醫學會",
+    "cat": "運動醫學",
+    "place": "原公告",
+    "url": "https://www.taiwanarthroscopy.org.tw/education/2026-07-26-%e4%bd%8f%e9%99%a2%e9%86%ab%e5%b8%ab%e8%81%af%e5%90%88%e6%95%99%e8%82%b2%e8%a8%93%e7%b7%b4%e8%aa%b2%e7%a8%8b/"
+  },
+  {
+    "date": "2026-08-15",
+    "title": "匯聚亞洲創傷骨科新視野，共啟臨床創新與國際交流新篇章",
+    "source": "TOTA 台灣骨科創傷醫學會",
+    "cat": "創傷",
+    "place": "原公告",
+    "url": "https://www.tota.org.tw/%e5%8c%af%e8%81%9a%e4%ba%9e%e6%b4%b2%e5%89%b5%e5%82%b7%e9%aa%a8%e7%a7%91%e6%96%b0%e8%a6%96%e9%87%8e%ef%bc%8c%e5%85%b1%e5%95%9f%e8%87%a8%e5%ba%8a%e5%89%b5%e6%96%b0%e8%88%87%e5%9c%8b%e9%9a%9b%e4%ba%a4/"
+  },
+  {
+    "date": "2026-08-15",
+    "title": "2026.08.15 大體膝關節鏡實作研習會 2026 Current Concepts in Knee Preservation- Precision PSI Around-the-Knee Osteotomy Cadaver Course",
+    "source": "台灣關節鏡及膝關節醫學會",
+    "cat": "運動醫學",
+    "place": "原公告",
+    "url": "https://www.taiwanarthroscopy.org.tw/conference/2026-08-15-%e5%a4%a7%e9%ab%94%e8%86%9d%e9%97%9c%e7%af%80%e9%8f%a1%e5%af%a6%e4%bd%9c%e7%a0%94%e7%bf%92%e6%9c%83-2026-current-concepts-in-knee-preservation-precision-psi-around-the-knee-osteotomy-cadav/"
+  },
+  {
+    "date": "2026-08-16",
+    "title": "【6/16報名】2026/08/16骨鬆專科醫師學分班(台南)(僅限本會醫師報名)",
+    "source": "台灣骨質疏鬆症學會",
+    "cat": "骨鬆",
+    "place": "原公告",
+    "url": "https://www.toa1997.org.tw/events/content.php?id=607&t=0"
+  },
+  {
+    "date": "2026-09-19",
+    "title": "【7/19開始報名(技術師班)】2026/09/19-20 ISCD Quality Bone Densitometry Course: Performance, Interpretation, and Clinical Application for Technologist ",
+    "source": "台灣骨質疏鬆症學會",
+    "cat": "骨鬆",
+    "place": "原公告",
+    "url": "https://www.toa1997.org.tw/events/content.php?id=610&t=0"
+  }
+];
